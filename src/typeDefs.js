979451359aa85e1a142e238d17506fc6f3872d16
Miu <miyuuu0728@gmail.com>
2021-08-15 22:28:33 -0700

    first commit

---
+++ src/typeDefs.js
@@ -0,0 +1,16 @@
+
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+type Query {
+    helloWorld: String!
+    dogs: [Dog!]!
+}
+type Dog {
+    id: ID!
+    name: String!
+}
+type Mutation {
+    createDog(name: String!): Dog!
+}
+`;
